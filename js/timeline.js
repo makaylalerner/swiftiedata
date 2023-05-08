3ba32a13117307314ad46575a9af609ea29f497c
Makayla Lerner <makayla.lerner@ourbranch.com>
2023-05-08 12:06:33 -0400

    timeline bug fix

--- js/timeline.js
+++ js/timeline.js
@@ -1,6 +1,6 @@
-/* d3.csv("https://gist.githubusercontent.com/makaylalerner/7e710a423a5157167c1b5b044ef85778/raw/ea194d1e0701346e62d2d5731c1dc8996a6eaa23/data_taylors_version2.csv").then((data) => {
+d3.csv("https://gist.githubusercontent.com/makaylalerner/7e710a423a5157167c1b5b044ef85778/raw/ea194d1e0701346e62d2d5731c1dc8996a6eaa23/data_taylors_version2.csv").then((data) => {
   createTimeline(data);
-}); */
+}); 
 
 export function createTimeline(data) {
     const nestedData = d3.group(data, (d) => d.era);
